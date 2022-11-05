85025c8f6169dbcbc23423ad24448881a60ca992
IvanMarakanov <marakanovivan@gmail.com>
2022-11-05 11:37:54 +0300

    add: шаблон хранилища режиссёров

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/director/DbDirectorStorage.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.storage.director;
+
+import ru.yandex.practicum.filmorate.model.Director;
+
+import java.util.Collection;
+
+public class DbDirectorStorage implements DirectorStorage {
+    @Override
+    public Collection<Director> getDirectors() {
+        return null;
+    }
+
+    @Override
+    public Director getDirectorById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Director addDirector(Director director) {
+        return null;
+    }
+
+    @Override
+    public Director updateDirector(Director director) {
+        return null;
+    }
+
+    @Override
+    public void removeDirectorById(Long id) {
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorStorage.java
@@ -0,0 +1,49 @@
+package ru.yandex.practicum.filmorate.storage.director;
+
+import ru.yandex.practicum.filmorate.model.Director;
+
+import java.util.Collection;
+
+public interface DirectorStorage {
+    /**
+     * Метод возвращает из хранилища всех
+     * режиссёров.
+     *
+     * @return Коллекция из всех режиссёров
+     * в хранилище
+     */
+    Collection<Director> getDirectors();
+
+    /**
+     * Метод возвращает режиссёра по его
+     * идентификатору.
+     *
+     * @param id идентификатор режиссёра.
+     * @return Режиссёр принадлежащий
+     * идентификатору.
+     */
+    Director getDirectorById(Long id);
+
+    /**
+     * Метод добавляет режиссёра в хранилище.
+     *
+     * @param director добавляемый режиссёр.
+     * @return Добавленный режиссёр.
+     */
+    Director addDirector(Director director);
+
+    /**
+     * Метод обновляет режиссёра в хранилище.
+     *
+     * @param director обновлённый режиссёр.
+     * @return Обновлённый режиссёр.
+     */
+    Director updateDirector(Director director);
+
+    /**
+     * Метод удаляет режиссёра из хранилища.
+     *
+     * @param id идентификатор режиссёра.
+     */
+    void removeDirectorById(Long id);
+}
