eff32fe77a5be45d1c2177b8d61f48e387d069a0
IvanMarakanov <marakanovivan@gmail.com>
2022-11-05 11:14:45 +0300

    add: модель Director

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Director {
+    @NotNull
+    private Long id;
+    @NotNull
+    private String name;
+}
