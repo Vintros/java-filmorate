8e7a40d0a08c3912303e4574214461b807dc1977
IvanMarakanov <marakanovivan@gmail.com>
2022-11-05 11:26:04 +0300

    add: шаблон DirectorService

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/DirectorService.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Director;
+
+import java.util.Collection;
+
+@Service
+@Slf4j
+public class DirectorService {
+    public Collection<Director> getDirectors() {
+        return null;
+    }
+
+    public Director getDirectorById(Long id) {
+        return null;
+    }
+
+    public Director addDirector(Director director) {
+        return null;
+    }
+
+    public Director updateDirector(Director director) {
+        return null;
+    }
+    public void removeDirectorById(Long id) {
+    }
+}
