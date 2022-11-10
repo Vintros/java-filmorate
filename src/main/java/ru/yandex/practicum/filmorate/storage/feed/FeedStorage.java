e7404be8ee4e421b20c6ccd10470aa84bfbe1df0
IvanMarakanov <marakanovivan@gmail.com>
2022-11-10 08:01:20 +0300

    add: комментарии для интерфейса FeedStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/feed/FeedStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/feed/FeedStorage.java
@@ -5,8 +5,20 @@ import ru.yandex.practicum.filmorate.model.Event;
 import java.util.List;
 
 public interface FeedStorage {
-
+    /**
+     * Метод записывает событие в хранилище.
+     *
+     * @param event объект события.
+     */
     void saveUserEvent(Event event);
 
+    /**
+     * Метод возвращает список событий
+     * связанных с пользователем.
+     *
+     * @param id идентификатор пользователя.
+     * @return Список всех событий связанных
+     * с пользователем.
+     */
     List<Event> getFeed(Long id);
 }
